a98aa1460d88fe865193a216987cf09e35a71d53
WebModerna <edgardogalletto@gmail.com>
2020-04-19 20:06:06 -0300

    Creación virtual del mazo de cartas. Cada palo incluso los comodines.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,33 @@
 // Creación del mazo de cartas
-const mazoHtml = document.getElementById("mazoCartas");
-const palos = ["oro", "espada", "copa", "basto", "comodines"];
+const mazoCartas = document.getElementById("mazoCartas");
+const palos = ["oro", "espada", "copa", "basto", "comodin"];
+let cantidadPalo = 12;
+const cantidadComodines = 2;
 function creacionMazohtml()
-{
-    let carta = document.createElement("div");
-    carta.setAttribute("id", "oro");
-    carta.innerText = "Oro";
-    mazoHtml.appendChild(carta);
+{ 
+    // Creación del mazo en baso a los palos 
+    for(let i = 0; i < palos.length; i++)   
+    {
+        // carta.setAttribute("id", palos[i]);
+        // mazoCartas.appendChild(carta);
+        // carta.innerText = palos[i];
+        
+        // Creación de las cartas de cada palo
+        for(let b = 0; b < cantidadPalo; b++)
+        {
+            let carta = document.createElement("div");
+            carta.setAttribute("id", palos[i]+"-"+(b+1))
+            mazoCartas.appendChild(carta);
+            carta.innerText = palos[i] + " - " + (b+1);
+            console.log(carta);
+            
+            // Los comodines
+            if(palos[i] === palos[palos.length-1])
+            {
+                cantidadPalo = cantidadComodines;
+            }
+        }
+    }
 }
 
 // Escuchadores de eventos.
