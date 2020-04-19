49048f2302be0c8eb555164ea231102d86a3f598
WebModerna <edgardogalletto@gmail.com>
2020-04-19 20:32:53 -0300

    Primeras estrucutraciones del layout.

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,13 @@ const mazoCartas = document.getElementById("mazoCartas");
 const palos = ["oro", "espada", "copa", "basto", "comodin"];
 let cantidadPalo = 12;
 const cantidadComodines = 2;
+
+
+// Creación del mazo en baso a los palos 
 function creacionMazohtml()
 { 
-    // Creación del mazo en baso a los palos 
     for(let i = 0; i < palos.length; i++)   
-    {
-        // carta.setAttribute("id", palos[i]);
-        // mazoCartas.appendChild(carta);
-        // carta.innerText = palos[i];
-        
+    {        
         // Creación de las cartas de cada palo
         for(let b = 0; b < cantidadPalo; b++)
         {
@@ -21,7 +19,7 @@ function creacionMazohtml()
             carta.innerText = palos[i] + " - " + (b+1);
             console.log(carta);
             
-            // Los comodines
+            // Controlar que los comodines son menor cantidad de cartas
             if(palos[i] === palos[palos.length-1])
             {
                 cantidadPalo = cantidadComodines;
